0a162ae30fe861ef773a73e7ace4d99d302f4473
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2021-04-23 17:12:07 +0700

    Update sigit.py

--- sigit.py
+++ sigit.py
@@ -422,7 +422,7 @@ class Facebook():
                 name = data["name"]
                 print(f"{space}{B} DONE {w} ID: {id} {r}->{w} {name}")
                 listid.append(data["id"])
-                f.write(id+"|"+data['id']+"|"+name+"\n")
+                f.write(id+"|"+name+"\n")
             except KeyboardInterrupt:
                 break
         f.close()
